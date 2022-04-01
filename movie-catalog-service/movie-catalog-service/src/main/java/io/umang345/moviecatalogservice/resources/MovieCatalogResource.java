473a6ed8ffbe8b7e120589687ef0a334334368eb
Umang <umangagarwal345@gmail.com>
2022-04-01 18:04:06 +0530

    Changed hardcoded links to dynamic links for service discovery

--- movie-catalog-service/movie-catalog-service/src/main/java/io/umang345/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/io/umang345/moviecatalogservice/resources/MovieCatalogResource.java
@@ -29,13 +29,13 @@ public class MovieCatalogResource
 	{ 
 		
 		//Get all rated movie IDS
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/"+userId
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/"+userId
 				, UserRating.class);
 		
 		return ratings.getUserRating().stream().map(rating -> 
 		{
 			//For each movie Id, call movie info service and get details
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 			
 			//Put them all together
 			return new CatalogItem(movie.getName(),"Desc",rating.getRating());
