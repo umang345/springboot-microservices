8c8ea4b2c3beb33ab494790dd4db5acaf7ab8709
Umang <umangagarwal345@gmail.com>
2022-04-01 14:33:57 +0530

    Added code for Synchronous call from movie catalog service to movie info service

--- movie-catalog-service/movie-catalog-service/src/main/java/io/umang345/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/movie-catalog-service/src/main/java/io/umang345/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,14 +1,19 @@
 package io.umang345.moviecatalogservice.resources;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.umang345.moviecatalogservice.models.CatalogItem;
+import io.umang345.moviecatalogservice.models.Movie;
+import io.umang345.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -17,9 +22,43 @@ public class MovieCatalogResource
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
 	{
-		 return Collections.singletonList(
-				 	 new CatalogItem("Transformers","Test",4)
-				 );
+		RestTemplate restTemplate = new RestTemplate();
+		
+		//Get all rated movie IDS
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234",2),
+				new Rating("5678",8)
+		);
+		
+		return ratings.stream().map(rating -> 
+		{
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+				return new CatalogItem(movie.getName(),"Desc",rating.getRating());
+		})
+		.collect(Collectors.toList());
+		
+		//For each movie Id, call movie info service and get details
+		
+		
+		
+		
+		//Put them all together
+		 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
